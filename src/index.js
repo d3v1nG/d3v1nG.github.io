06beb66ace023d1abf3302eefd1edcf3dffc6514
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2020-01-15 17:20:49 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page from './Page.js';
+
+const page = <Page />;
+
+ReactDOM.render(
+  page,
+  document.getElementById('root')
+);
