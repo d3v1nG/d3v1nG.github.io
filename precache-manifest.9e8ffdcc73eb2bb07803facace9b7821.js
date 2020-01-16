8d35b134dfefeee74dd561b33e9657a1680d2934
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2020-01-16 10:18:37 -0600

    Updates

---
+++ precache-manifest.9e8ffdcc73eb2bb07803facace9b7821.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40790bca4b310f77c517f23746f8f348",
+    "url": "/my_page/index.html"
+  },
+  {
+    "revision": "6339bbea6e8ab6fd65ab",
+    "url": "/my_page/static/css/main.59e40d6f.chunk.css"
+  },
+  {
+    "revision": "43f3d8a3b6c9f7f33a2b",
+    "url": "/my_page/static/js/2.fff06b4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_page/static/js/2.fff06b4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6339bbea6e8ab6fd65ab",
+    "url": "/my_page/static/js/main.8eb8f8a0.chunk.js"
+  },
+  {
+    "revision": "a06b16cd30d9b71ed13d",
+    "url": "/my_page/static/js/runtime-main.aa5702ad.js"
+  },
+  {
+    "revision": "b59be83f4a2dabeabe81a29ebd40656c",
+    "url": "/my_page/static/media/Devin_Gluesing_Resume.b59be83f.docx"
+  },
+  {
+    "revision": "a3a70418b69298c056a8cfede1b85cf2",
+    "url": "/my_page/static/media/city8bit.a3a70418.jpg"
+  },
+  {
+    "revision": "01c9512de0c82184f622e6b30960d3e2",
+    "url": "/my_page/static/media/city_red.01c9512d.png"
+  },
+  {
+    "revision": "b2a22516e32e954d17da7e14ae0c7489",
+    "url": "/my_page/static/media/night_stars.b2a22516.gif"
+  },
+  {
+    "revision": "418f72bb6a64ce2d53984697c62825cd",
+    "url": "/my_page/static/media/pp2.418f72bb.jpg"
+  }
+]);
